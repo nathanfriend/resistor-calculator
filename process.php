c4df5f30711ff1796fb85c3c4bdaaf47ff26799c
Nathan Friend <n.friend@cant-col.ac.uk>
2012-05-18 14:59:25 +0100

    Initial commit.

---
+++ process.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+switch ($_POST['band1']) {
+    case brown:
+        echo "1";
+        break;
+    case red:
+        echo "2";
+        break;
+}
+
+
+
+
+
+
+?>
